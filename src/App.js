4c01cab617a17cb2eb2a1c74f22936c0a920fae8
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-09-17 20:29:04 +0530

    "Removed App.css and refactored App.js to display a step-by-step guide with buttons and messages. Added styles to index.css for the new layout."

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <p className="message">Hello</p>
+      <div className="buttons">
+        <button
+          style={{
+            backgroundColor: "#7950f2",
+            color: "#fff",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Prev
+        </button>
+        <button
+          style={{
+            backgroundColor: "#7950f2",
+            color: "#fff",
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
